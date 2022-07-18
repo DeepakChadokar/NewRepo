ac0ebcbf769e49c70e79b30d083e02aac6e0b43f
NIKHIL RAJ <NIKHIL RAJ@DESKTOP-J9EO0C6>
2022-07-18 17:38:45 +0530

    okk sir gggggg

--- SampleMavenProject/src/test/java/SampleTest/DinoGame.java
+++ SampleMavenProject/src/test/java/SampleTest/DinoGame.java
@@ -4,10 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class DinoGame {
 	
-	public static void main(String[] args) {
+@Test
+public void dano()
+ {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/search?q=github&rlz=1C1VDKB_enIN1007IN1007&oq=git&aqs=chrome.1.69i57j0i271l2j69i60l2.8263j0j15&sourceid=chrome&ie=UTF-8");
---
+++ SampleMavenProject/src/test/java/SampleTest/Try.java
@@ -0,0 +1,12 @@
+package SampleTest;
+
+import org.testng.annotations.Test;
+
+public class Try
+{
+	@Test
+	public void trial()
+	{
+		System.out.println("trial");
+	}
+}
